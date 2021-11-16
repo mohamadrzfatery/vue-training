f679b8c8a71bd900d050dabd8a40487d1ac1c26b
mohammadrzfaetry <fatery66@gmail.com>
2021-11-16 18:31:53 +0330

    add lifecycle

---
+++ pages/life.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>life cycle methods</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy')
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+}
+</script>
+
+<style></style>

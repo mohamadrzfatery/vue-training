8935aa5c81957b995d6e7170a75030d9239ee341
mohammadrzfaetry <fatery66@gmail.com>
2021-11-16 17:58:32 +0330

    install nuxt and reactivity and props train

---
+++ components/HomeCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>counter is {{ counter }}</p>
+    <button @click="counter++">plus</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialCounter: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      counter: this.initialCounter,
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-header></home-header>
+    <home-counter :initial-counter="initial" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      initial: 4,
+    }
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>

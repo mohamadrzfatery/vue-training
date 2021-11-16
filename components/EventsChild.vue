57e5ad6bc11d25df145edb7910f988df3caf9d1a
mohammadrzfaetry <fatery66@gmail.com>
2021-11-16 18:55:12 +0330

    add event

---
+++ components/EventsChild.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>event child</p>
+    <button @click="submit">submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    submit() {
+      setTimeout(() => {
+        this.$emit('onSuccess', 'my custom param')
+      }, 1000)
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/events.vue
@@ -0,0 +1,15 @@
+<template>
+  <events-child @onSuccess="onSuccess"></events-child>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSuccess(param) {
+      console.log('success', param)
+    },
+  },
+}
+</script>
+
+<style></style>

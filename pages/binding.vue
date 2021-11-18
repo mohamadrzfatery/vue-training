3fcf24095c1219f08900a912869ce497898ec28b
mohammadrzfaetry <fatery66@gmail.com>
2021-11-18 18:43:26 +0330

    class and style binding

---
+++ pages/binding.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>class and style binding</h1>
+    <div :class="[{ active: isActive }, 'b']" @click="toggle" class="binding">
+      binding
+    </div>
+    <div :style="{ color: 'yellow' }">style binding</div>
+    {{ isActive.toString() }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive
+    },
+  },
+}
+</script>
+
+<style scoped>
+.binding {
+  width: 200px;
+  height: 200px;
+  background-color: rebeccapurple;
+}
+.active {
+  background-color: royalblue;
+}
+</style>

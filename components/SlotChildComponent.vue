aaf0f2749e138c2a0965a7337fde079ae3620089
mohammadrzfaetry <fatery66@gmail.com>
2021-11-18 19:24:00 +0330

    slots

---
+++ components/SlotChildComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>dialog</h1>
+    <div class="bg-grey">
+      <slot></slot>
+    </div>
+    <slot name="actionButtons" param1="param1"></slot>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.bg-grey {
+  background-color: grey;
+}
+</style>
---
+++ pages/slots.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>slots</h1>
+    <slot-child-component>
+      <div class="title">dialog content</div>
+      <template #actionButtons="{ param1 }">
+        <span>{{ param1 }}</span>
+        <button class="button1">button1</button>
+        <button class="button2">button2</button>
+      </template>
+    </slot-child-component>
+  </div>
+</template>
+
+<script>
+import SlotChildComponent from '~/components/SlotChildComponent.vue'
+export default {
+  components: { SlotChildComponent },
+}
+</script>
+
+<style scoped>
+.title {
+  color: red;
+}
+.button1 {
+  color: rosybrown;
+}
+.button2 {
+  color: saddlebrown;
+}
+</style>

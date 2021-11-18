e155e7076179b61cec5cbdda9fd1226baabe40e0
mohammadrzfaetry <fatery66@gmail.com>
2021-11-18 23:37:20 +0330

    parent - note : do not use $parent in ur code because its naghzing capsulation

--- pages/ref-parent.vue
+++ pages/ref-parent.vue
@@ -3,6 +3,11 @@
     <h1>ref-parent</h1>
     <ref-parent-child ref="myRef"></ref-parent-child>
     <button @click="resetChild">reset from parent</button>
+    <hr />
+    <ref-parent-child ref="myRef"></ref-parent-child>
+    <div>
+      <span>parentCount : {{ parentCount }}</span>
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,11 @@
 import RefParentChild from '~/components/RefParentChild.vue'
 export default {
   components: { RefParentChild },
+  data() {
+    return {
+      parentCount: 0,
+    }
+  },
   //   beforeMount() {
   //     this.$refs.myRef.reset() // in harekat eshtebast va baes error mishe nabayad to beforemount ino bezani
   //   },

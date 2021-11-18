96a74ff541c75097a308b6d663fa16ea124a7680
mohammadrzfaetry <fatery66@gmail.com>
2021-11-19 00:05:50 +0330

    routing infinit params

---
+++ pages/search/_.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>search</h1>
+    <ul>
+      <li v-for="(item, index) in getParams" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route.params)
+  },
+  computed: {
+    getParams() {
+      return this.$route.params.pathMatch.split('/')
+    },
+  },
+}
+</script>
+
+<style></style>

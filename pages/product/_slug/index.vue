6a25aa70d8d2e8761e143da96d4d802aeda06ae6
mohammadrzfaetry <fatery66@gmail.com>
2021-11-18 23:51:04 +0330

    routing and params

---
+++ pages/product/_slug/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>product</h1>
+    <div>slug is : {{ this.$route.params.slug }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route.params)
+  },
+}
+</script>
+
+<style></style>

4d34266de2120100d69a952f2a3e13cda4fb3537
mohammadrzfaetry <fatery66@gmail.com>
2021-11-18 23:13:05 +0330

    watch

---
+++ pages/watch.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>watch</h1>
+    <button @click="count++">plus</button>
+    <button @click="changeObject">change</button>
+    <div>
+      {{ myObject }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      myObject: {
+        child: { a: 1 },
+      },
+    }
+  },
+  methods: {
+    changeObject() {
+      this.myObject.child.a++
+    },
+  },
+  watch: {
+    count: {
+      handler(value, oldvalue) {
+        console.log('value', value, 'oldvalue', oldvalue)
+      },
+      immediate: true,
+    },
+    myObject: {
+      handler(value) {
+        console.log('myvalue', value)
+      },
+      deep: true, //yani taghiraate dakhel object ro neshoon bede
+    },
+    'myObject.child.a'(value) {
+      console.log('value', value)
+    },
+
+    // count(value, oldvalue) {
+    //   console.log('value', value, 'oldvalue', oldvalue)
+    // },
+  },
+}
+</script>
+
+<style></style>

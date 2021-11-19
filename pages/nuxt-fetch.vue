cbdf56c27bb841774ad6af32f9c6711076ada79f
mohammadrzfaetry <fatery66@gmail.com>
2021-11-19 20:26:54 +0330

    errorHandleing

--- pages/nuxt-fetch.vue
+++ pages/nuxt-fetch.vue
@@ -3,6 +3,9 @@
     <h1>nuxt fecth</h1>
     <div v-if="$fetchState.pending">loading...</div>
     <!-- farghesh ba async date ine ke ba fetch mishe to safe khodesh loading gozasht vali ba asynce nemishe va ta mogheii ke data ro az server nagerefte nemire to safe va fetch ro mitoni to component estefade koni vali async nemishe-->
+    <p v-else-if="$fetchState.error">
+      {{ $fetchState.error.message }}
+    </p>
     <p v-else>
       {{ title }}
     </p>
@@ -13,12 +16,18 @@
 export default {
   fetch() {
     return this.$axios
-      .$get('https://jsonplaceholder.typicode.com/todos/1')
+      .$get('https://jsonplaceholder.typicode.com/todos/0')
       .then((response) => {
         this.title = response.title
       })
       .catch((e) => {
-        console.log(e)
+        const statusCode = e?.response?.status || 500
+        const message = e?.response?.statusText || 'oops error'
+        // if (process.server) {
+        //   this.$nuxt.context.res.statusCode = statusCode         // age mikhay safe error biad
+        // }
+        // this.$nuxt.error({ statusCode, message })
+        throw new Error(`${message} (${statusCode})`) // age mikhay to hamoon safe dar yek khat error ro neshoonet bede va nare besafe error
       })
   },
   data() {

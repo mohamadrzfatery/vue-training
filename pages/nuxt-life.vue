8470f1e3944a94f4da4ade7f90c0d694f080e2d8
mohammadrzfaetry <fatery66@gmail.com>
2021-11-19 19:49:56 +0330

    asyncData&ssrConcepts

---
+++ pages/nuxt-life.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>ssr and nust lifeCycle methods</h1>
+    {{ name }}
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'ali',
+    }
+  },
+  async asyncData({ $axios }) {
+    const response = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/todos/1'
+    )
+    return { title: response.title }
+  },
+}
+</script>
+
+<style></style>

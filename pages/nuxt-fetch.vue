dec2925675616796874b3f3f6a13195a17d8385c
mohammadrzfaetry <fatery66@gmail.com>
2021-11-19 20:06:32 +0330

    fetch&ssr

---
+++ pages/nuxt-fetch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>nuxt fecth</h1>
+    <div v-if="$fetchState.pending">loading...</div>
+    <!-- farghesh ba async date ine ke ba fetch mishe to safe khodesh loading gozasht vali ba asynce nemishe va ta mogheii ke data ro az server nagerefte nemire to safe va fetch ro mitoni to component estefade koni vali async nemishe-->
+    <p v-else>
+      {{ title }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  fetch() {
+    return this.$axios
+      .$get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        this.title = response.title
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  },
+  data() {
+    return {
+      title: '',
+    }
+  },
+}
+</script>
+
+<style></style>

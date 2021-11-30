a638d4ab79755c7c3f54486c03d6aabe274eaf67
mohammadrzfaetry <fatery66@gmail.com>
2021-11-30 14:21:30 +0330

    validate

---
+++ pages/validate/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>validate</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params, error }) {
+    error({ statusCode: 500, message: 'خطایی رخ داده است' })
+    return /^\d+$/.test(params.id) // ghable (+) yani age test kone bebine addade ya na
+  },
+}
+</script>
+
+<style></style>
